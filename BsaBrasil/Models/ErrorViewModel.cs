b9f75350c6a5f490dce0b4c7b4f2874de7b354b1
Lucas de Carli Ramos Costa Humenhuk <lucas.decarli@outlook.com>
2018-05-10 15:48:41 -0300

    Add project files.

---
+++ BsaBrasil/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BsaBrasil.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
