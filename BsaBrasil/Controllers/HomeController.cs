6c370197d5ec272d01323d2b8470009ec4fe4975
Lucas de Carli Ramos Costa Humenhuk <lucas.decarli@outlook.com>
2018-05-18 00:18:11 -0300

    porrada de coisas

--- BsaBrasil/Controllers/HomeController.cs
+++ BsaBrasil/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace BsaBrasil.Controllers
             return View();
         }
 
+        public IActionResult Home()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
--- BsaBrasil/Startup.cs
+++ BsaBrasil/Startup.cs
@@ -43,7 +43,7 @@ namespace BsaBrasil
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Home}/{id?}");
             });
         }
     }
