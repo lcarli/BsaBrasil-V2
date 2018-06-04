23192ac17b43dce7982d3f73889960eff5a1e804
Lucas de Carli Ramos Costa Humenhuk <lucas.decarli@outlook.com>
2018-06-04 14:45:51 -0300

    testando localization

--- BsaBrasil/Controllers/HomeController.cs
+++ BsaBrasil/Controllers/HomeController.cs
@@ -5,11 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BsaBrasil.Models;
+using Microsoft.Extensions.Localization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Builder;
 
 namespace BsaBrasil.Controllers
 {
     public class HomeController : Controller
     {
+        public IStringLocalizer<HomeController> Localizer { get; }
+
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            Localizer = localizer;
+        }
         public IActionResult Index()
         {
             return View();
@@ -22,8 +33,6 @@ namespace BsaBrasil.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
--- BsaBrasil/Startup.cs
+++ BsaBrasil/Startup.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace BsaBrasil
 {
@@ -21,7 +25,10 @@ namespace BsaBrasil
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.AddMvc()
+                    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
