864910339c64adcab72e552dc1e8155b6e756310
Lucas de Carli Ramos Costa Humenhuk <lucas.decarli@outlook.com>
2018-06-05 11:21:49 -0300

    test for CI/CD

--- BsaBrasil/Controllers/HomeController.cs
+++ BsaBrasil/Controllers/HomeController.cs
@@ -28,8 +28,6 @@ namespace BsaBrasil.Controllers
 
         public IActionResult Home()
         {
-            //var rqf = Request.HttpContext.Features.Get<IRequestCultureFeature>();
-            //ViewData["CC"] = rqf.RequestCulture.Culture;
             return View();
         }
 
