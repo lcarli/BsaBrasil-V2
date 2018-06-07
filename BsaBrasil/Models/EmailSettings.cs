070a78f2d1d95726608c3d29fd0c2ee701092bf0
Lucas de Carli Ramos Costa Humenhuk <lucas.decarli@outlook.com>
2018-06-07 15:05:33 -0300

    Cleaning email class

--- BsaBrasil/Models/EmailSettings.cs
+++ BsaBrasil/Models/EmailSettings.cs
@@ -11,10 +11,6 @@ namespace BsaBrasil.Models
 
         public int PrimaryPort { get; set; }
 
-        public String SecondayDomain { get; set; }
-
-        public int SecondaryPort { get; set; }
-
         public String UsernameEmail { get; set; }
 
         public String UsernamePassword { get; set; }
